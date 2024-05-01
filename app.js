e95981eb515e56b1d86f47bd2d52c432f7c8c62d
arifprdn <arifpradana315@gmail.com>
2024-05-01 19:54:12 +0700

    update

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.set('view engine', 'ejs');
 
 const routes = require('./routes');
 app.use('/api/v1', routes);
-app.get('/', (req, res) => res.render('image'));
+app.get('/');
 
 // 500 error handler
 app.use((err, req, res, next) => {
